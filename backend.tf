1b72352df1571ef65e641f14b48c1694bfc8fd2e
Sridhar <sridhar@myopenwardrobe.com>
2024-10-20 22:21:58 -0700

    fix4

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rgakskubent"
-    storage_account_name = "tfstatestorage"
+    storage_account_name = "svtfstatestorage99"
     container_name       = "tfstatestoragecont"
     key                  = "tfstate_file.tfstate"
     tenant_id            = "16b3c013-d300-468d-ac64-7eda0820b6d3"
