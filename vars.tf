35ffc986a3e76e7290b4aae20fe8052a4cc58809
Sridhar <sridhar@myopenwardrobe.com>
2024-10-21 20:30:48 -0700

    format

--- vars.tf
+++ vars.tf
@@ -9,34 +9,38 @@ variable "container_name" {
   description = "The name of the Blob Storage container."
   default     = "tfstatestoragecont"
 }
+
+# Input variable: Name of the Public IP
 variable "name" {
   description = "The name of the Public IP."
   type        = string
-  default = "sv-my-public-ip"
+  default     = "sv-my-public-ip"
 }
 
+# Input variable: Location/Region
 variable "location" {
   description = "The location/region where the Public IP is created."
   type        = string
   default     = "East US"
 }
 
+# Input variable: Resource Group Name
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the Public IP."
   type        = string
   default     = "rgakskubent"
 }
 
+# Input variable: Allocation Method
 variable "allocation_method" {
   description = "Defines the allocation method for the Public IP. Possible values are Static or Dynamic."
   type        = string
   default     = "Static"
 }
-
 variable "sku" {
   description = "The SKU of the Public IP. Possible values are Basic or Standard."
   type        = string
-  default     = "STandard"
+  default     = "Standard"
 }
 
 variable "tags" {
