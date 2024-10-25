b24bb6a91e6bff92341b55b1d2b4131ccbfe603c
Sridhar <sridhar@myopenwardrobe.com>
2024-10-24 19:58:46 -0700

    refactor

---
+++ backend-setup.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "backend" {
+  name     = "rgakskubent"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "backend" {
+  name                     = "svtfstatestorage99"
+  resource_group_name      = azurerm_resource_group.backend.name
+  location                 = azurerm_resource_group.backend.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "my-terraform-env"
+  }
+}
+
+resource "azurerm_storage_container" "backend" {
+  name                  = "tfstatestoragecont"
+  storage_account_name  = azurerm_storage_account.backend.name
+  container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "storage_account_key_operator" {
+  principal_id         = "84f6f1be-759f-4225-b540-e87ee015ea2c"
+  role_definition_name = "Storage Account Key Operator Service Role"
+  scope                = azurerm_storage_account.backend.id
+}
--- main.tf
+++ main.tf
@@ -1,32 +1,20 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rgakskubent"
+    storage_account_name = "svtfstatestorage99"
+    container_name       = "tfstatestoragecont"
+    key                  = "tfstate_file.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 # Create a Resource Group if it doesn’t exist
 resource "azurerm_resource_group" "tfexample" {
   name     = var.resource_group_name
   location = var.location
-}
-
-# Create a Storage account
-resource "azurerm_storage_account" "terraform_state" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.tfexample.name
-  location                 = azurerm_resource_group.tfexample.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "my-terraform-env"
-  }
-}
-
-# Create a Storage container
-resource "azurerm_storage_container" "terraform_state" {
-  name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.terraform_state.name
-  container_access_type = "private"
-}
-resource "azurerm_role_assignment" "storage_account_key_operator" {
-  principal_id   = "84f6f1be-759f-4225-b540-e87ee015ea2c"
-  role_definition_name = "Storage Account Key Operator Service Role"
-  scope          = azurerm_storage_account.terraform_state.id
 }
 
 module "public_ip" {
