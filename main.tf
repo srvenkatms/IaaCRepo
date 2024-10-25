9664e63aea32f65305ac87657ff75a6e3d14ced7
Sridhar <sridhar@myopenwardrobe.com>
2024-10-25 12:55:24 -0700

    commit

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ terraform {
 module "public_ip" {
   source              = "./terraform-modules/public-ip"
   name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.workloadrg.location
+  resource_group_name = azurerm_resource_group.workloadrg.name
   allocation_method   = var.allocation_method
   sku                 = var.sku
   tags = {
--- vars.tf
+++ vars.tf
@@ -16,7 +16,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "The name of the resource group in which to create the Public IP."
   type        = string
-  default     = "rgrgdeviaac"
+  default     = "rgdeviaac"
 }
 
 # Input variable: Allocation Method
